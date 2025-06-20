0fc9f56bc92f8a369873565535ebe40d4d360560
asafeeson <g.kochergin@hotmail.com>
2025-06-20 15:43:42 +0200

    - modal

--- src/routes/prizes/second/+page.svelte
+++ src/routes/prizes/second/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import Button from '$lib/components/Button.svelte';
+	import { fade } from 'svelte/transition';
 	import type { PageData } from './$types';
 
 	const { data }: { data: PageData } = $props();
+	let showKefirModal = $state<Boolean>(false);
 </script>
 
-<div class="flex flex-col h-dvh px-6 pb-[10%]">
+<div class="flex flex-col h-dvh px-6 pb-[10%] relative">
 	<div
 		class="flex flex-col justify-center items-center bg-secondary rounded-b-4xl overflow-hidden gap-6 min-h-[471px] w-full py-6"
 	>
@@ -28,6 +30,25 @@
 		</p>
 	</div>
 	<div class="flex h-full justify-between items-center">
-		<Button url="/prizes/choose" name="забрать приз"></Button>
+		<button
+			class="w-full h-16 bg-button grid place-content-center rounded-4xl uppercase font-bold text-2xl cursor-pointer"
+			onclick={() => (showKefirModal = !showKefirModal)}
+		>
+			забрать приз
+		</button>
 	</div>
+
+	{#if showKefirModal}
+		<div
+			class="grid place-content-center w-[300px] h-auto py-6 px-6 bg-white shadow-2xl absolute z-100 rounded-2xl top-1/2 left-1/2 -translate-x-1/2 border-2 border-white"
+			transition:fade={{ delay: 250, duration: 300 }}
+
+		>
+			<span>В этот момент Пользователь регистрируется на кефире</span>
+			<a
+				href="/prizes/choose"
+				class="bg-main uppercase px-4 py-2 w-full rounded-2xl text-center"
+			>далее --&gt;</a>
+		</div>
+	{/if}
 </div>
