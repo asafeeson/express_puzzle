af794dc2d94faef31615b7ee49e60bf184875c80
asafeeson <g.kochergin@hotmail.com>
2025-06-20 14:34:53 +0200

    - upd

--- src/routes/prizes/fortune/+page.svelte
+++ src/routes/prizes/fortune/+page.svelte
@@ -55,11 +55,12 @@
 	{#if showPop}
 		<div
 			class="w-full h-1/2 absolute bottom-0 left-0 px-4"
-			transition:fly={{ delay: 1400, duration: 400, x: 0, y: 400, opacity: 0.5, easing: quintOut }}
+			transition:fly={{ delay: 1400, duration: 400, x: 0, y: 1000, opacity: 0.5, easing: quintOut }}
 		>
-			<div class="bg-secondary h-full w-full py-10 rounded-t-2xl opacity-80 justify-center items-center flex flex-col">
+			<div class="bg-secondary h-full w-full p-10 rounded-t-2xl opacity-90 justify-between items-center flex flex-col">
 				<h2 class="font-bold">Вы выиграли</h2>
 				<p>Описания приза</p>
+                <Button url="/" name="забрать приз"></Button>
 			</div>
 		</div>
 	{/if}
