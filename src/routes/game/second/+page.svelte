29c7341a19b3f8819a3e9c721f5c255ae96bbf28
asafeeson <g.kochergin@hotmail.com>
2025-06-20 14:56:46 +0200

    - overlay

--- src/routes/game/second/+page.svelte
+++ src/routes/game/second/+page.svelte
@@ -10,7 +10,7 @@
 		class="flex flex-col justify-center items-center bg-secondary rounded-b-4xl overflow-hidden p-4 gap-6 min-h-[471px]"
 	>
 		<div class="overflow-hidden rounded-2xl aspect-square">
-			<img src="/big_puzzle_image.png" alt="" class="h-full w-auto object-fill" />
+			<img src="/big_puzzle_overlay.png" alt="" class="h-full w-auto object-fill" />
 		</div>
 		<p class="text-center text-xl">Собери элементы пазла</p>
 	</div>
