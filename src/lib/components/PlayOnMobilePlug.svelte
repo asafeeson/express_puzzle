0d6cc74344bad3220cdee2acad2da217e1c5b4c0
asafeeson <g.kochergin@hotmail.com>
2025-07-12 11:59:50 +0200

    - replace image to video plug for desktop

--- src/lib/components/PlayOnMobilePlug.svelte
+++ src/lib/components/PlayOnMobilePlug.svelte
@@ -1,7 +1,13 @@
-<main class="hidden sm:flex max-w-xl mx-auto h-full w-full flex-col justify-center items-center gap-6">
+<main
+	class="hidden sm:flex max-w-xl mx-auto h-full w-full flex-col justify-center items-center gap-6"
+>
 	<h1>Играй с Телефона</h1>
 	<div class="flex relative">
-		<enhanced:img src="/static/desktop.png" alt="Играй с Телефона, cканируй QR код"></enhanced:img>
+		<video autoplay loop muted playsinline>
+			<source src="/desktop-plug.webm" type="video/webm" />
+			Ваш браузер не поддерживает видео.
+		</video>
+		<!-- <enhanced:img src="/static/desktop.png" alt="Играй с Телефона, cканируй QR код"></enhanced:img> -->
 		<h2 class="self-end w-10 absolute bottom-0 right-0">Сканируй QR&nbsp;код</h2>
 	</div>
 </main>
